39c9f5306119d8d1c63849efd5f1369c1b226239
rajendranneil19 <rajendranneil19@gmail.com>
2018-04-10 12:21:30 -0400

    got index route for bags

--- app/controllers/api/bags_controller.rb
+++ app/controllers/api/bags_controller.rb
@@ -1,2 +1,29 @@
 class Api::BagsController < ApplicationController
-end
+    
+
+    def index 
+        @bags = User.find(params[:user_id]).bags.order(id: :desc)
+        render json: @bags
+       
+            end 
+        
+            
+            def show 
+               
+        
+    
+                
+            end 
+        
+            def create 
+            end 
+        
+            def update
+            end 
+        
+            def destroy 
+            end 
+        end
+            
+        
+
