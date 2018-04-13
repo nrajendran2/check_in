b3d5d38bd81dee6d5d1bfd5723474faf77fe0edd
rajendranneil19 <rajendranneil19@gmail.com>
2018-04-13 14:39:43 -0400

    Found healthbuddies for reference of the update

--- client/src/components/EditBag.js
+++ client/src/components/EditBag.js
@@ -2,13 +2,48 @@ import React, {Component} from 'react';
 
 
 class EditBag extends Component {
+
+    state = {
+        bag: {
+            bagname: '',
+            weight:'',
+            fragile: ''
+
+        }
+    }
+
+  
+    handleChange = (event) => {
+        const editState = { ...this.state.user }
+        editState[event.target.name] = event.target.value
+
+        this.setState({ user: editState })
+    }
+
+    submitNewUserName = (event) => {
+        event.preventDefault()
+        const payload = {
+            bagname: this.state.user.bagname,
+            weight: this.state.user.weight,
+            fragile: this.state.user.fragile
+
+        }
+
+        axios.patch(`/api/users/${this.props.userId.userid}`, payload).then((res) => {
+
+            console.log(res.data)
+            this.setState({ user: res.data })
+            window.location.reload()
+
+        })
+    }
+
     render() {
         return (
             <div>
 
                 <form action="">
-                    <input type ="text"
-                    name = "bagname"/>
+                    <input type ="text" name = "bagname"/>
 
                      <input type ="text"
                     name = "weight"/>
